ded6aca50a117cd7cd34786662dc72c83e3c0944
Alexander Milosh <milosh.alexander.by@gmail.com>
2024-02-25 01:19:53 +0100

    Add SprResilience4J Circuit Breaker

--- order-service/src/main/java/pl/amilosh/orderservice/controller/OrderController.java
+++ order-service/src/main/java/pl/amilosh/orderservice/controller/OrderController.java
@@ -1,6 +1,11 @@
 package pl.amilosh.orderservice.controller;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,8 +15,10 @@ import pl.amilosh.orderservice.dto.OrderDto;
 import pl.amilosh.orderservice.service.OrderService;
 
 import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.HttpStatus.SERVICE_UNAVAILABLE;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
+@Slf4j
 @RestController
 @RequestMapping("/order")
 @RequiredArgsConstructor
@@ -21,7 +28,15 @@ public class OrderController {
 
     @PostMapping(produces = APPLICATION_JSON_VALUE)
     @ResponseStatus(CREATED)
-    public OrderDto createOrder(@RequestBody OrderDto orderDto) {
-        return orderService.createOrder(orderDto);
+    @CircuitBreaker(name = "inventory", fallbackMethod = "fallbackMethod")
+    @TimeLimiter(name = "inventory")
+    @Retry(name = "inventory")
+    public ResponseEntity<Object> createOrder(@RequestBody OrderDto orderDto) {
+        var createdOrder = orderService.createOrder(orderDto);
+        return new ResponseEntity<>(createdOrder, CREATED);
+    }
+
+    public ResponseEntity<Object> fallbackMethod(OrderDto orderDto, RuntimeException ex) {
+        return new ResponseEntity<>("Oops! Something went wrong, please order after some time 4!", SERVICE_UNAVAILABLE);
     }
 }
